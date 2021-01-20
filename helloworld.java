b7fea302113e40618de395c9adc31d1639927b4e
fassali <40204274+fassali@users.noreply.github.com>
2021-01-20 18:02:25 +0100

    error syntax

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public  class  HelloWorld {
   public  static  void  main ( String [] args ) {
-    System.out.println( " Bonjour tout le monde! " );
+    System.out.println( "Bonjour tout le monde!" );
   }
 }
